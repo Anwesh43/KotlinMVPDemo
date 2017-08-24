b24c95ce9177d9cab01d47cd763fe48ed63f2240
Anwesh43 <anweshthecool0@gmail.com>
2017-08-24 16:04:50 +0530

    adding CircularLoaderHolder

--- app/src/main/java/com/anwesome/ui/kotlinmvpdaggerdemo/model/CircularLoaderHolder.kt
+++ app/src/main/java/com/anwesome/ui/kotlinmvpdaggerdemo/model/CircularLoaderHolder.kt
@@ -1,8 +1,26 @@
 package com.anwesome.ui.kotlinmvpdaggerdemo.model
 
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.RectF
+
 /**
  * Created by anweshmishra on 24/08/17.
  */
 class CircularLoaderHolder:IModel {
-
+    data class CircularLoader(var x:Float,var y:Float,var scale:Float = 1.0f){
+        fun draw(canvas:Canvas,paint:Paint) {
+            var r = Math.min(canvas.width,canvas.height).toFloat()/20
+            paint.color = Color.parseColor("#009688")
+            canvas.save()
+            canvas.translate(x,y)
+            canvas.drawArc(RectF(-r,-r,r,r),0f,360f*scale,true,paint)
+            canvas.restore()
+        }
+        fun update() {
+            scale -= 0.1f
+        }
+        fun stopped():Boolean = scale == 0f
+    }
 }
--- app/src/main/java/com/anwesome/ui/kotlinmvpdaggerdemo/presenter/CircularLoaderHolderPresenter.kt
+++ app/src/main/java/com/anwesome/ui/kotlinmvpdaggerdemo/presenter/CircularLoaderHolderPresenter.kt
@@ -3,13 +3,14 @@ package com.anwesome.ui.kotlinmvpdaggerdemo.presenter
 import android.view.View
 import com.anwesome.ui.kotlinmvpdaggerdemo.model.CircularLoaderHolder
 import com.anwesome.ui.kotlinmvpdaggerdemo.model.IModel
+import com.anwesome.ui.kotlinmvpdaggerdemo.views.CircularLoaderHolderView
 
 /**
  * Created by anweshmishra on 24/08/17.
  */
 class CircularLoaderHolderPresenter:IPresenter {
     override fun setView(v:View) {
-
+        var view = v as CircularLoaderHolderView
     }
     override fun getModel():IModel {
         return CircularLoaderHolder()
